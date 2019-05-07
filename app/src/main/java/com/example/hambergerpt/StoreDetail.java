d780d4a0b9dd4c68619d3fa60d0645c77f30d0fc
alldifferent <alldifferent@naver.com>
2019-05-08 01:13:23 +0900

    디테일 주문정보 버거이름과 가격수정 남음

--- app/src/main/java/com/example/hambergerpt/StoreDetail.java
+++ app/src/main/java/com/example/hambergerpt/StoreDetail.java
@@ -1,19 +1,26 @@
 package com.example.hambergerpt;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.hambergerpt.adapters.MenuAdapter;
 import com.example.hambergerpt.databinding.ActivityStoreDetailBinding;
 import com.example.hambergerpt.datas.HambMenu;
 import com.example.hambergerpt.datas.HambergerStores;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,6 +74,40 @@ public class StoreDetail extends BaseActivity {
             }
         });
 
+        act.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PermissionListener permissionListener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+
+                        hambergerStoresList = (HambergerStores)getIntent().getSerializableExtra("가게정보");
+                        String callStr = String.format("tel:%s",hambergerStoresList.phoneNum);
+                        Uri uri = Uri.parse(callStr);
+                        Intent intent = new Intent(Intent.ACTION_CALL,uri);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+
+                        Toast.makeText(mContext, "권한이 거부되었습니다.\n"+deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                };
+
+                TedPermission
+                        .with(StoreDetail.this)
+                        .setPermissionListener(permissionListener)
+                        .setDeniedMessage("만약 권한을 거부하면 서비스를 이용 할 수 없습니다.\n이용하시려면 설정에서 권한을 부여하십시오.")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+            }
+        });
+
     }
 
     @Override
@@ -81,13 +122,30 @@ public class StoreDetail extends BaseActivity {
         int indexNum = act.menuSpinner.getSelectedItemPosition();
         hambMenuData = mList.get(indexNum);
 
-
         Glide.with(StoreDetail.this).load(imgUri).into(act.storeImg);
         act.menuNameTxt.setText(hambergerStoresList.storeName);
 
-        int price = hambMenuData.menuPrice;
-        menuAdapter.notifyDataSetChanged();
-        act.payBtn.setText(String.format("결제하기(%d원)",price));
+        act.deliveryRdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int price = hambMenuData.menuPrice;
+                act.takeOrPickTxt.setText("배달 이용");
+                act.payBtn.setText(String.format("결제하기(%d원)",price+2000));
+                act.showMenuTxt.setText(hambMenuData.menuName);
+                menuAdapter.notifyDataSetChanged();
+            }
+        });
+
+        act.pickRdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int price = hambMenuData.menuPrice;
+                act.takeOrPickTxt.setText("방문수령 이용");
+                act.payBtn.setText(String.format("결제하기(%d원)",price));
+                act.showMenuTxt.setText(hambMenuData.menuName);
+                menuAdapter.notifyDataSetChanged();
+            }
+        });
 
 
 
